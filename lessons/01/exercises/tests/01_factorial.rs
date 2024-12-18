bf84b5e8c844e73522ff68549ed2cb664650b806
Ganesh oli <ganesh@thaiofficefurniture.com>
2024-12-18 20:36:11 +0545

    update 01_exerice

--- lessons/01/exercises/tests/01_factorial.rs
+++ lessons/01/exercises/tests/01_factorial.rs
@@ -1,6 +1,19 @@
 //! Run this file with `cargo test --test 01_factorial`.
 
 // TODO: Implement a simple factorial function.
+pub fn factorial(n: u32) -> u32 {
+    (1..=n).product()
+}
+
+pub fn factorial_1(n: u32) -> u32 {
+    // Base case: factorial of 0 is 1
+    if n == 0 {
+        return 1;
+    }
+
+    // Recursive case: Calculate factorial using recursion
+    n * factorial(n - 1)
+}
 
 /// Below you can find a set of unit tests.
 #[cfg(test)]
